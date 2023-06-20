4d54629fca13a4b5caac24113e2b6fa257f27e27
Adefela <adefakorode@gmail.com>
2023-06-20 13:14:48 -0400

    solving

---
+++ mathFunction.js
@@ -0,0 +1,15 @@
+function addFunction(a,b){
+    return a+b;
+}
+
+function subtractFunction(a,b){
+    return a-b;
+}
+
+function multiplyFunction(a,b){
+    return a*b;
+}
+
+function divideFunction(a,b){
+    return a/b;
+}
